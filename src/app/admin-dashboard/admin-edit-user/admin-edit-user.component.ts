f6029b881eaff7ba9dd893b2201ec26bb3603bab
Barbu Dacian <73746297+Daci1@users.noreply.github.com>
2023-04-22 14:01:22 +0300

    TLF-9: adds new field inputs required to edit the trainer in both profile view and admin edit view (#10)

--- src/app/admin-dashboard/admin-edit-user/admin-edit-user.component.ts
+++ src/app/admin-dashboard/admin-edit-user/admin-edit-user.component.ts
@@ -5,6 +5,7 @@ import {UserService} from "../../services/user.service";
 import * as moment from "moment";
 import {ActivatedRoute, Router} from "@angular/router";
 import {User, UserRoles} from "../../shared/models";
+import {TrainerService} from "../../services/trainer.service";
 
 @Component({
   selector: 'app-admin-edit-user',
@@ -19,8 +20,15 @@ export class AdminEditUserComponent implements OnInit{
 
   userRolesControl = new FormControl<UserRoles[]>([], Validators.required);
   roles: string[] = Object.keys(UserRoles);
+  public isTrainer: boolean;
 
-  constructor(private authService: AuthService, private userService: UserService, private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private trainerService: TrainerService
+  ) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
@@ -35,17 +43,16 @@ export class AdminEditUserComponent implements OnInit{
       firstName: new FormControl(null, Validators.required),
       lastName: new FormControl(null, Validators.required),
       dob: new FormControl(null, Validators.required),
+      startOfDay: new FormControl(null),
+      endOfDay: new FormControl(null),
+      totalClientsPerReservation: new FormControl(null),
     });
-    this.userService.getUser(this.usernameToEdit).subscribe(user => {
-      this.user = user;
-      this.profileForm.setValue({
-        email: this.user.username,
-        firstName: this.user.firstName,
-        lastName: this.user.lastName,
-        dob: moment(this.user.dob, "DD-MM-YYYY").format("YYYY-MM-DD")
-      });
-      this.loadRolesForUser();
-    })
+    this.isTrainer = this.roles.includes(UserRoles.TRAINER);
+    if(this.isTrainer) {
+      this.loadProfileForTrainer();
+    } else {
+      this.loadProfileForUser();
+    }
   }
 
   onSubmit() {
@@ -53,7 +60,12 @@ export class AdminEditUserComponent implements OnInit{
       firstName: this.profileForm.value.firstName,
       lastName: this.profileForm.value.lastName,
       dob: moment(this.profileForm.value.dob, "YYYY-MM-DD").format("DD-MM-YYYY"),
-      newRoles: this.userRolesControl.value
+      newRoles: this.userRolesControl.value,
+      ...(this.isTrainer && { patchTrainerBody: {
+          startOfDay: moment(this.profileForm.value.startOfDay, 'hh:mm').format('hh:00:00'),
+          endOfDay: moment(this.profileForm.value.endOfDay, 'HH:mm').format('HH:00:00'),
+          totalClientsPerReservation: this.profileForm.value.totalClientsPerReservation
+        }}),
     }).subscribe(() => {
       this.initForm();
     });
@@ -79,6 +91,37 @@ export class AdminEditUserComponent implements OnInit{
 
   private loadRolesForUser() {
     this.userRolesControl.setValue(this.user.roles);
-    console.log(this.userRolesControl.dirty);
+  }
+
+  private loadProfileForTrainer() {
+    this.trainerService.getTrainerFullInfo(this.usernameToEdit).subscribe(trainer => {
+      this.user = trainer.user;
+      this.profileForm.setValue({
+        email: this.user.username,
+        firstName: this.user.firstName,
+        lastName: this.user.lastName,
+        dob: moment(this.user.dob, "DD-MM-YYYY").format("YYYY-MM-DD"),
+        startOfDay: trainer.startOfDay,
+        endOfDay: trainer.endOfDay,
+        totalClientsPerReservation: trainer.totalClientsPerReservation,
+      });
+      this.loadRolesForUser();
+    });
+  }
+
+  private loadProfileForUser() {
+    this.userService.getUser(this.usernameToEdit).subscribe(user => {
+      this.user = user;
+      this.profileForm.setValue({
+        email: this.user.username,
+        firstName: this.user.firstName,
+        lastName: this.user.lastName,
+        dob: moment(this.user.dob, "DD-MM-YYYY").format("YYYY-MM-DD"),
+        startOfDay: null,
+        endOfDay: null,
+        totalClientsPerReservation: 0,
+      });
+      this.loadRolesForUser();
+    });
   }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../auth/auth.service";
 import * as moment from "moment/moment";
 import {UserService} from "../services/user.service";
+import {TrainerService} from "../services/trainer.service";
 
 @Component({
   selector: 'app-profile',
@@ -13,9 +14,10 @@ export class ProfileComponent {
   profileForm: FormGroup;
 
   isFormDisabled: boolean = true;
+  isTrainer: boolean;
   private initialState: object;
 
-  constructor(private authService: AuthService, private userService: UserService) { }
+  constructor(private authService: AuthService, private userService: UserService, private trainerService: TrainerService) { }
 
   ngOnInit(): void {
     this.initForm();
@@ -28,17 +30,16 @@ export class ProfileComponent {
       firstName: new FormControl(null, Validators.required),
       lastName: new FormControl(null, Validators.required),
       dob: new FormControl(null, Validators.required),
+      startOfDay: new FormControl(null),
+      endOfDay: new FormControl(null),
+      totalClientsPerReservation: new FormControl(null),
     });
-    this.userService.getLoggedUser().subscribe(user => {
-      this.profileForm.setValue({
-        email: user.username,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        dob: moment(user.dob, "DD-MM-YYYY").format("YYYY-MM-DD")
-      });
-      this.profileForm.disable();
-      this.initialState = this.profileForm.value;
-    })
+    this.isTrainer = this.authService.isTrainerLogged();
+    if(this.isTrainer) {
+      this.loadProfileForTrainer();
+    } else {
+      this.loadProfileForUser();
+    }
   }
 
   onSubmit() {
@@ -52,9 +53,15 @@ export class ProfileComponent {
         lastName: this.profileForm.value.lastName,
         dob: moment(this.profileForm.value.dob, "YYYY-MM-DD").format("DD-MM-YYYY"),
         newRoles: [],
+        ...(this.isTrainer && { patchTrainerBody: {
+            startOfDay: moment(this.profileForm.value.startOfDay, 'hh:mm').format('hh:00:00'),
+            endOfDay: moment(this.profileForm.value.endOfDay, 'HH:mm').format('HH:00:00'),
+            totalClientsPerReservation: this.profileForm.value.totalClientsPerReservation
+          }}),
       }).subscribe(() => {
         this.initForm();
       });
+      console.log(this.profileForm.value);
     }
   }
 
@@ -66,5 +73,38 @@ export class ProfileComponent {
 
   private revertChanges() {
     this.profileForm.setValue(this.initialState);
+  }
+
+  private loadProfileForTrainer() {
+    const trainerUsername = this.authService.getLoggedUsername();
+    this.trainerService.getTrainerFullInfo(trainerUsername).subscribe(trainer => {
+      this.profileForm.setValue({
+        email: trainer.user.username,
+        firstName: trainer.user.firstName,
+        lastName: trainer.user.lastName,
+        dob: moment(trainer.user.dob, "DD-MM-YYYY").format("YYYY-MM-DD"),
+        startOfDay: trainer.startOfDay,
+        endOfDay: trainer.endOfDay,
+        totalClientsPerReservation: trainer.totalClientsPerReservation
+      });
+      this.profileForm.disable();
+      this.initialState = this.profileForm.value;
+    })
+  }
+
+  private loadProfileForUser() {
+    this.userService.getLoggedUser().subscribe(user => {
+      this.profileForm.setValue({
+        email: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        dob: moment(user.dob, "DD-MM-YYYY").format("YYYY-MM-DD"),
+        startOfDay: null,
+        endOfDay: null,
+        totalClientsPerReservation: 0,
+      });
+      this.profileForm.disable();
+      this.initialState = this.profileForm.value;
+    });
   }
 }
